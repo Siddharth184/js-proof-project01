0ee80d26ac6bb50ef5db505b454a580e64fc8554
Siddharth184 <157238127+Siddharth184@users.noreply.github.com>
2024-06-02 23:05:00 +0530

    Create js_assessment01.js

---
+++ js_assessment01.js
@@ -0,0 +1,54 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const nftcollection =[];
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (name, gender,country) {
+    let nft ={
+        name: name,
+        gender: gender,
+        country: country
+    };
+    nftcollection.push(nft);
+
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+    for(let i =0; i<nftcollection.length; i++){
+        console.log("Name: "+ nftcollection[i].name);
+        console.log("Gender: "+ nftcollection[i].gender);
+        console.log("Nationallity: "+ nftcollection[i].country);
+        console.log("----------------------------------");
+    }
+
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    console.log("Total NFTs: "+ nftcollection.length);
+
+}
+
+// call your functions below this line
+
+mintNFT("Siddharth","male","India");
+mintNFT("Antonia","female","USA");
+mintNFT("lucas","male","Sweden");
+mintNFT("Benjamin","male","France");
+
+listNFTs();
+
+getTotalSupply();
